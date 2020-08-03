cb7ec63c59e4b13a5265b7c8a45a9efc358ae163
mbalanya <okobacharles@yahoo.co.uk>
2020-08-04 02:04:04 +0300

    add feedback_fragment_mood_dialog

---
+++ app/src/main/java/com/chalookoba/lyrikapp/FeedbackDialogFragment.java
@@ -0,0 +1,18 @@
+package com.chalookoba.lyrikapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.DialogFragment;
+
+public class FeedbackDialogFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.feedback_fragment_mood_dialog, container, false);
+        getDialog().setTitle("Lyrics Feedback");
+        return rootView;
+    }
+}
