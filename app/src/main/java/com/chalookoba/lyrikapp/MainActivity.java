82392f9c0bfc16e0357d8562361d5a8f44da5581
mbalanya <okobacharles@yahoo.co.uk>
2020-08-03 10:55:00 +0300

    Add button Logic and toast

--- app/src/main/java/com/chalookoba/lyrikapp/MainActivity.java
+++ app/src/main/java/com/chalookoba/lyrikapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.chalookoba.lyrikapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSearchSongButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchSongButton = (Button)findViewById(R.id.searchSongButton);
+        mSearchSongButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
