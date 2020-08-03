98c0df6c997871a51ccb605c2b24c5910e911ba4
mbalanya <okobacharles@yahoo.co.uk>
2020-08-03 12:07:47 +0300

    add an array of artists

--- app/src/main/java/com/chalookoba/lyrikapp/TrendingSongsActivity.java
+++ app/src/main/java/com/chalookoba/lyrikapp/TrendingSongsActivity.java
@@ -8,6 +8,11 @@ import android.widget.TextView;
 
 public class TrendingSongsActivity extends AppCompatActivity {
     private TextView mSearchArtistTextView;
+    private String[] artists = new String[] {"Juice WRLD", "The Chicks",
+            "Pop Smoke", "Harry Styles", "Lil Baby", "Post Malone",
+            "DaBaby", "Luke Combs", "The Weekend", "BTS",
+            "Drake", "Jhene Aiko", "Roddy Ricch",
+            "Dua Lipa", "Morgan Wallen", "Lewis Capaldi", "Dj Khaled", "Billie Eilish", "Megan Thee Stallion", "Justin Bieber"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
