cce567a7e7bb2186917f8bdc06ba7114cc15e711
mbalanya <okobacharles@yahoo.co.uk>
2020-08-04 01:42:31 +0300

    implement interface for onclick in MainActivity

--- app/src/main/java/com/chalookoba/lyrikapp/MainActivity.java
+++ app/src/main/java/com/chalookoba/lyrikapp/MainActivity.java
@@ -8,15 +8,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     public static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.searchSongButton) Button mSearchSongButton;
     @BindView(R.id.artistSearch) EditText mArtistSearch;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +27,16 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toast.makeText(MainActivity.this, "Welcome to LyrikApp World!", Toast.LENGTH_LONG).show();
 
-        mSearchSongButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String artistSearch = mArtistSearch.getText().toString();
-                Intent intent = new Intent(MainActivity.this, TrendingSongsActivity.class);
-                intent.putExtra("artistSearch", artistSearch);
-                startActivity(intent);
-            }
-        });
+        mSearchSongButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSearchSongButton){
+            String artistSearch = mArtistSearch.getText().toString();
+            Intent intent = new Intent(MainActivity.this, TrendingSongsActivity.class);
+            intent.putExtra("artistSearch", artistSearch);
+            startActivity(intent);
+        }
     }
 }
