c39e01464f4369d63d8cf280d24ff01c42eadff9
mbalanya <okobacharles@yahoo.co.uk>
2020-08-04 03:12:09 +0300

    add logic to cancel and submit buttons

--- app/src/main/java/com/chalookoba/lyrikapp/FeedbackDialogFragment.java
+++ app/src/main/java/com/chalookoba/lyrikapp/FeedbackDialogFragment.java
@@ -1,9 +1,13 @@
 package com.chalookoba.lyrikapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.fragment.app.DialogFragment;
 
@@ -13,6 +17,28 @@ public class FeedbackDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.feedback_fragment_dialog, container, false);
         getDialog().setTitle("Lyrics Feedback");
-        return rootView;
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+        RadioGroup feedbackRadioGroup = (RadioGroup) rootView.findViewById(R.id.feedbackRadioGroup);
+        int selectedId = feedbackRadioGroup.getCheckedRadioButtonId();
+        final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId);
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("testing", selectedRadioButton.getText().toString());
+                dismiss();
+            }
+        });
+    return rootView;
     }
 }
